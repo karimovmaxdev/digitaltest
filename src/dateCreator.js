d8e624c2572f6ce858c0e46536be4d860bb915f4
Maxim Karimov <Juk3z1973@gmail.com>
2022-04-07 02:59:18 +0500

    minor update

--- src/dateCreator.js
+++ src/dateCreator.js
@@ -46,23 +46,23 @@ export function correctMonths() {
 
     allMonth.push(calendar[i]);
   }
-  
+
     allMonth.reverse();
     return allMonth
 }
 
 
-  export function dateFormatter(date) {
-    const year = date.getFullYear().toString()
-    let month = (date.getMonth() + 1).toString()
-    let day = date.getDate().toString()
-    
-    if (month.length === 1) {
-      month = `0${month}`
-    }
+export function dateFormatter(date) {
+  const year = date.getFullYear().toString()
+  let month = (date.getMonth() + 1).toString()
+  let day = date.getDate().toString()
+  
+  if (month.length === 1) {
+    month = `0${month}`
+  }
 
-    if (day.length === 1) {
-      day = `0${day}`
-    }
-    return `${year}-${month}-${day}`;
+  if (day.length === 1) {
+    day = `0${day}`
+  }
+  return `${year}-${month}-${day}`;
   }
