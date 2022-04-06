ce46eb5566b7532c914af039738f3963ca420710
Maxim Karimov <Juk3z1973@gmail.com>
2022-04-07 02:08:10 +0500

    minor

--- src/service/fetchAPI.js
+++ src/service/fetchAPI.js
@@ -14,7 +14,7 @@ export async function getDates(staticDates) {
       if (findedObject !== undefined) {
         findedObject.contr = value;
       }
-      }
+    }
       
 
     return staticDates;
