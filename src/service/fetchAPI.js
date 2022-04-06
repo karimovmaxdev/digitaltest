32b69a868a2ea296417970ed3d6091aa767fefd3
Maxim Karimov <Juk3z1973@gmail.com>
2022-04-07 02:49:34 +0500

    minor

--- src/service/fetchAPI.js
+++ src/service/fetchAPI.js
@@ -1,21 +1,21 @@
 import { dateFormatter } from "../dateCreator";  
 export async function getDates(staticDates) {
       
-    const response = await fetch(
-      "https://test.digitalpartnersglobal.com/test/calendar.json"
-    );
-    const dates = await response.json();
+  const response = await fetch(
+    "https://test.digitalpartnersglobal.com/test/calendar.json"
+  );
+  const dates = await response.json();  
 
-    for (const [key, value] of Object.entries(dates)) {
-      const stringDate = new Date(key);
-      const formatedDate = dateFormatter(stringDate)
+  for (const [key, value] of Object.entries(dates)) {
+    const stringDate = new Date(key);
+    const formatedDate = dateFormatter(stringDate)
 
-      if (staticDates[formatedDate] === undefined) {
-        continue
-      }
-      
-      staticDates[formatedDate] = value
+    if (staticDates[formatedDate] === undefined) {
+      continue
     }
+    
+    staticDates[formatedDate] = value
+  }
       
   const result = Object.entries(staticDates).reverse()
     return result;
